44edc79fd0601100276f6bace25064bb09f6abcb
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-12-30 22:57:45 +0800

    feat: add User model

--- models/Idea.js
+++ models/Idea.js
@@ -2,24 +2,11 @@ import mongoose from 'mongoose';
 
 const ideaSchema = new mongoose.Schema(
   {
-    title: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    summary: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    description: {
-      type: String,
-      required: true,
-    },
-    tags: {
-      type: [String],
-      default: [],
-    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true, trim: true },
+    summary: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    tags: { type: [String], default: [] },
   },
   {
     timestamps: true,
