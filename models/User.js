52a265865f39a90c7162070aaf343ed17820447e
Kristopher Mendoza <creative.kristopher@gmail.com>
2025-12-31 01:18:31 +0800

    feat: add and integrate authMiddleware

--- models/User.js
+++ models/User.js
@@ -28,11 +28,11 @@ const userSchema = new mongoose.Schema(
 
 // Hash passwords before saving
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next;
+  if (!this.isModified('password')) return next();
 
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next;
+  next();
 });
 
 // Compare entered password with hashed password
