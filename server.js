ce9cff1ae267c21d636db039b8982332d3a6d7e8
Kristopher Mendoza <creative.kristopher@gmail.com>
2026-01-02 23:51:03 +0800

    feat: add production domain to cors

--- server.js
+++ server.js
@@ -21,19 +21,7 @@ const allowedOrigins = [
   'https://idea-drop-ui-sooty.vercel.app',
 ];
 
-// app.use(cors({ origin: allowedOrigins, credentials: true }));
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 console.log(origin);
 
